492f77dac830f484d3d7fa527b0e914c3a6c02d4
azimut <azimut.github@protonmail.com>
2024-03-23 06:32:42 -0300

    progress...added a bit more of types

--- frontend/index.js
+++ frontend/index.js
@@ -2,10 +2,9 @@ import { createSQLiteThread, createHttpBackend } from 'sqlite-wasm-http';
 
 export async function initConnection() {
   const remoteURL = 'http://127.0.0.1/feeds.db';
-  //const remoteURL = 'https://velivole.b-cdn.net/maptiler-osm-2017-07-03-v3.6.1-europe.mbtiles';
   const httpBackend = createHttpBackend({
     //maxPageSize: 1024,
-    maxPageSize: 4096,
+    maxPageSize: 1024,
     timeout: 10000,
     cacheSize: 4096
   });
@@ -17,28 +16,29 @@ export async function initConnection() {
       });
       return db;
     });
+  // db('close', {}); // await
+  // db.close();
+  // httpBackend.close(); // await
 }
 
-export async function getDomains(dbarg) {
+export async function getFeeds(dbarg) {
   let db = await dbarg;
   console.log(db);
   let queue = [];
-  await db('exec', { // await
-    sql: 'SELECT title FROM feeds LIMIT 3',
-    // sql: 'SELECT zoom_level FROM tiles LIMIT 3',
+  await db('exec', {
+    sql: 'SELECT id, title FROM feeds',
     bind: {},
     callback: (msg) => {
       if (msg.row) {
-        queue.push(msg.row[0]);
-        // console.log(msg.columnNames);
-        // console.log(msg.row);
+        let [id,title] = msg.row;
+        queue.push({
+          id: id,
+          title: title
+        });
       }
     }
   });
   return queue;
-  // db('close', {}); // await
-  // db.close();
-  // httpBackend.close(); // await
 }
 
 export async function getEntries(dbarg, feedid) {
@@ -48,7 +48,6 @@ export async function getEntries(dbarg, feedid) {
     sql: `SELECT id, title, date, url
           FROM entries
           WHERE feedid=$fid
-          LIMIT 3
     `,
     bind: {$fid: feedid},
     callback: (msg) => {
--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -1,7 +1,9 @@
 port module Main exposing (..)
 
 import Browser
+import Dict exposing (Dict)
 import Html exposing (Html, article, details, div, summary, text)
+import Html.Events exposing (onClick)
 
 
 main : Program () Model Msg
@@ -14,10 +16,18 @@ main =
         }
 
 
-port receiveNewFeeds : (List String -> msg) -> Sub msg
+type alias Model =
+    { feeds : List Feed
+    , entries : Dict Int (List Entry)
+    }
 
 
-port receiveNewEntries : (List Entry -> msg) -> Sub msg
+type alias Feed =
+    { id : Int
+    , title : String
+    , description : String
+    , isVisible : Bool
+    }
 
 
 type alias Entry =
@@ -26,48 +36,104 @@ type alias Entry =
     , title : String
     , date : String
     , url : String
+    , description : String
+    , content : String
+    , isVisible : Bool
     }
 
 
 type Msg
-    = NewRawFeeds (List String)
-    | NewRawEntries (List Entry)
+    = InitFeeds (List InitFeed)
+    | AskForEntries Int
+    | NewEntries (List NewEntry)
 
 
-type alias Feed =
-    { title : String
+type alias InitFeed =
+    { id : Int
+    , title : String
     }
 
 
-type alias Model =
-    { feeds : List Feed
+type alias NewEntry =
+    { id : Int
+    , feedid : Int
+    , title : String
+    , date : String
+    , url : String
     }
+
+
+port askForEntries : Int -> Cmd msg
+
+
+port receiveEntries : (List NewEntry -> msg) -> Sub msg
+
+
+port receiveInitFeeds : (List InitFeed -> msg) -> Sub msg
 
 
 init : flags -> ( Model, Cmd msg )
 init _ =
-    ( Model [ Feed "foo", Feed "bar" ], Cmd.none )
+    ( Model [] Dict.empty, Cmd.none )
+
+
+newEntry : NewEntry -> Entry
+newEntry { id, feedid, title, date, url } =
+    { id = id
+    , feedid = feedid
+    , title = title
+    , date = date
+    , url = url
+    , description = ""
+    , content = ""
+    , isVisible = True
+    }
+
+
+newEntries : Model -> List NewEntry -> Model
+newEntries ({ entries } as model) nes =
+    case nes of
+        [] ->
+            model
+
+        entry :: _ ->
+            { model | entries = Dict.insert entry.feedid (List.map newEntry nes) entries }
+
+
+initFeeds : List InitFeed -> Model
+initFeeds ifs =
+    { feeds = List.map initFeed ifs
+    , entries = Dict.empty
+    }
+
+
+initFeed : InitFeed -> Feed
+initFeed { id, title } =
+    { id = id, title = title, description = "", isVisible = True }
 
 
 update : Msg -> Model -> ( Model, Cmd msg )
 update msg model =
     case msg of
-        NewRawFeeds feeds ->
-            ( Model <| List.map Feed feeds, Cmd.none )
+        AskForEntries feedid ->
+            ( model, askForEntries feedid )
 
-        NewRawEntries _ ->
-            ( model, Cmd.none )
+        InitFeeds feeds ->
+            ( initFeeds feeds, Cmd.none )
+
+        NewEntries entries ->
+            ( newEntries model entries, Cmd.none )
 
 
-feedView : Feed -> Html msg
-feedView { title } =
-    article []
+feedView : Feed -> Html Msg
+feedView { title, id } =
+    article [ onClick (AskForEntries id) ]
         [ details []
             [ summary [] [ text title ] ]
         ]
 
 
-view : Model -> Html msg
+view : Model -> Html Msg
 view { feeds } =
     div [] (List.map feedView feeds)
 
@@ -75,6 +141,6 @@ view { feeds } =
 subscriptions : model -> Sub Msg
 subscriptions model =
     Sub.batch
-        [ receiveNewFeeds NewRawFeeds
-        , receiveNewEntries NewRawEntries
+        [ receiveInitFeeds InitFeeds
+        , receiveEntries NewEntries
         ]
