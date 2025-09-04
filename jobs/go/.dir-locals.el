38db5d303e2e6421d865e9624e85b7d2dc0af3a8
azimut <azimut.github@protonmail.com>
2025-09-04 16:07:33 -0300

    dev: dirlocals update, make it more generic

--- jobs/go/.dir-locals.el
+++ jobs/go/.dir-locals.el
@@ -1,14 +1,14 @@
 ((go-mode . ((dape-configs . ((go-debug-newspod-gofetch
                                modes (go-mode go-ts-mode)
                                command "dlv"
-                               command-args ("dap" "--listen" "127.0.0.1:55878")
+                               command-args ("dap" "--listen" "127.0.0.1::autoport")
                                command-cwd default-directory
                                host "127.0.0.1"
-                               port 55878
+                               port :autoport
                                :buildFlags "-tags 'sqlite_fts5 sqlite_foreign_keys'"
                                :request "launch"
                                :mode "debug"
                                :type "go"
                                :showLog "true"
-                               :cwd "/home/sendai/projects/go/newspod/jobs/go/"
+                               :cwd (concat default-directory "/..") ; "/src/.."
                                :program "./..."))))))
